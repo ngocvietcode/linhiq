0dc7e51d0ae6649b9ec0ec66f50f1c7160f4100b
Ngoc Viet Bui <ngocviet@linhiq.com>
2026-04-14 00:18:24 +0700

    fix: next 16 promotes outputFileTracingRoot out of experimental

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -4,9 +4,7 @@ import path from "path";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, "../../"),
-  },
+  outputFileTracingRoot: path.join(__dirname, "../../"),
 };
 
 export default nextConfig;
